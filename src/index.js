80b5bffc74eec1a9ee015cebf83bf1a8fd270f1d
Keylor <keylordani@gmail.com>
2024-04-24 12:07:53 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import chalk from 'chalk';
+
+const app = express();
+
+const PORT = process.env.API_PORT || 3000;
+// const whiteList = ['https://app.teodrive.com', 'http://localhost:5173'];
+
+app.use(helmet());
+app.use(express.json());
+// app.use(cors({ origin: '*' }));
+
+// Not Found route
+app.use((req, res, next) => {
+  return res.status(404).send('404\nNot Found');
+});
+
+// Server listening
+app.listen(PORT, () => {
+  console.log(
+    chalk.black.bgGreenBright.bold(` Running \n`) +
+      chalk.yellowBright(`🚀 Server listening on port `) +
+      chalk.magentaBright.bold(`${PORT}`)
+  );
+});
